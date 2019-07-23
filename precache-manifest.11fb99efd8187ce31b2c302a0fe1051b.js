ffedd6eb6613464a5787b3d4bce5190b3484837f
Somesh Kar <someshkar04@gmail.com>
2019-07-23 20:04:57 +0530

    Updates

---
+++ precache-manifest.11fb99efd8187ce31b2c302a0fe1051b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c5607c1dd733d493d0dc4f3502b68c",
+    "url": "/gateway19-prelim/index.html"
+  },
+  {
+    "revision": "cf4b85d0d66fea3f2f34",
+    "url": "/gateway19-prelim/static/css/main.5b5b2c3e.chunk.css"
+  },
+  {
+    "revision": "8a3dc11395b6890e8bf1",
+    "url": "/gateway19-prelim/static/js/2.db25d78a.chunk.js"
+  },
+  {
+    "revision": "cf4b85d0d66fea3f2f34",
+    "url": "/gateway19-prelim/static/js/main.040de93b.chunk.js"
+  },
+  {
+    "revision": "5746b1d994f48ed6efad",
+    "url": "/gateway19-prelim/static/js/runtime~main.e1d634d4.js"
+  }
+]);
